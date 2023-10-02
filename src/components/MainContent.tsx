0d48ffe2254e5181b973e2816746f123233d38af
Yash Kansara <82724814+yash06112002@users.noreply.github.com>
2023-10-02 16:22:59 +0530

    Added Card

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Layout } from "antd";
+import { Layout } from "antd";
 import MainNav1 from "./MainNav1";
 import StatusBar from "./StatusBar";
 import CardComp from "./Card";
