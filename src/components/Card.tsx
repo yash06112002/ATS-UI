8be265424b59cf6fd8e610a775aef9b3f1128da0
Yash Kansara <82724814+yash06112002@users.noreply.github.com>
2023-10-02 16:21:18 +0530

    Added Card

---
+++ src/components/Card.tsx
@@ -0,0 +1,134 @@
+import React from "react";
+import { Avatar, Checkbox, Layout } from "antd";
+const { Content } = Layout;
+// import { Dropdown } from "antd";
+
+const CardComp: React.FC = () => {
+  return (
+    <Content style={{ padding: "30px 50px" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            margin: "0 16px 0 0x",
+          }}
+        >
+          <Checkbox />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            margin: "0 50px 0 10px",
+          }}
+        >
+          <Avatar
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              margin: "0 0px 0 25px",
+              scale: "2",
+            }}
+          />
+        </div>
+        <div
+          style={{
+            width: 500,
+            display: "flex",
+            flexDirection: "column",
+            margin: "0 10px",
+          }}
+        >
+          <div
+            style={{
+              width: 500,
+              display: "flex",
+              flexDirection: "row",
+              margin: "0 20px",
+              border: "none",
+            }}
+          >
+            <div
+              style={{
+                width: 500,
+                display: "flex",
+                flexDirection: "column",
+                margin: "0 20px",
+              }}
+            >
+              <p style={{ width: 250, fontSize: "20px", fontWeight: "bold" }}>
+                Aaliyah Sanderson
+              </p>
+              <p style={{ width: 250, fontSize: "15px", fontWeight: "bold" }}>
+                Riyadh, Saudi Arabia
+              </p>
+              <p>Bachelor - Cambridge University (2023 - 2023)</p>
+              <div
+                style={{
+                  width: 250,
+                  display: "flex",
+                  flexDirection: "row",
+                  margin: "0 10px",
+                  justifyContent: "space-around",
+                  color: "blue",
+                }}
+              >
+                <p>#top_candidate</p>
+                <p>#top_candidate</p>
+              </div>
+              <div
+                style={{
+                  width: 250,
+                  display: "flex",
+                  flexDirection: "row",
+                  margin: "0 10px",
+                  justifyContent: "space-around",
+                }}
+              >
+                <p
+                  style={{
+                    padding: "0 10px",
+                    borderRadius: "10px",
+                    color: "#007a78",
+                    backgroundColor: "#c4fff7",
+                  }}
+                >
+                  New York
+                </p>
+                <p
+                  style={{
+                    padding: "0 10px",
+                    borderRadius: "10px",
+                    color: "#007a78",
+                    backgroundColor: "#c4fff7",
+                  }}
+                >
+                  Marketing
+                </p>
+                <p
+                  style={{
+                    padding: "0 10px",
+                    borderRadius: "10px",
+                    color: "#007a78",
+                    backgroundColor: "#c4fff7",
+                  }}
+                >
+                  London
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Content>
+  );
+};
+
+export default CardComp;
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { Layout } from "antd";
+import { Card, Layout } from "antd";
 import MainNav1 from "./MainNav1";
 import StatusBar from "./StatusBar";
+import CardComp from "./Card";
 
 const MainContent: React.FC = () => {
   return (
     <Layout className="layout">
       <MainNav1 />
       <StatusBar />
+      <CardComp />
+      <CardComp />
     </Layout>
   );
 };
